7cd9354763e4a888bcc1976821d1ab7c469e3b6b
Rajesh S <rajbca00@gmail.com>
2024-05-17 21:19:24 +0530

    implemented sqllite in froshims

--- froshims/app.py
+++ froshims/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
-REGISTRANTS = {}
-
 SPORTS = ['Basketball','Soccer','Ultimate Frisbee']
+DATABASE = 'froshims.db'
+
+
+def get_db():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    return conn, cursor
+
+def close_db(conn, cursor):
+    cursor.close()
+    conn.close()
 
 @app.route("/")
 def index():
@@ -23,11 +33,34 @@ def register():
         return render_template("index.html",sports=SPORTS , error="Invalid sport")
     
     # Remember regustrant
-    REGISTRANTS[name] = sport
+    conn, cursor = get_db()
+    
+    cursor.execute("INSERT INTO registrants (name, sport) VALUES (?, ?)", (name, sport))
+    conn.commit()
+
+    close_db(conn, cursor)
 
     # Confirm registration
     return redirect("/registrants")
 
 @app.route("/registrants")
 def registrants():
-    return render_template("registrants.html", registrants=REGISTRANTS)
+    conn, cursor = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM registrants")
+    registrants = cursor.fetchall()
+    print(registrants)
+    close_db(conn, cursor)
+    return render_template("registrants.html", registrants=registrants)
+
+@app.route("/deregister", methods=['POST'])
+def deregister():
+    id = request.form.get('id')
+    print(id)
+    if id:
+        conn, cursor = get_db()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM registrants WHERE id=?",id)
+        conn.commit()
+        close_db(conn, cursor)
+    return redirect("/registrants")
